39843c787d2000bc9067038f5a5b75e92fcf08a8
khushboo400 <khushboo171400.cse@chitkara.edu.in>
2019-12-20 23:58:22 +0530

    Added state in 'AddSubscriber' and 'App' components

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import './App.css';
-class App extends Component {
-  render() {
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    }
+  }
+
+  render() {
     return (
       <div>
         <Header heading="Phone Directory" />
@@ -26,20 +21,22 @@ class App extends Component {
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
           {
-            subscribers.map(sub => {
-              return <div key={sub.id} className="grid-container">
-                <span className="grid-item">{sub.name}</span>
-                <span className="grid-item">{sub.phone}</span>
-                <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
-                </span>
-              </div>
-            })
-          }
+               this.state.subscribersListToShow.map(sub => {
+                return <div key={sub.id} className="grid-container">
+                  <span className="grid-item">{sub.name}</span>
+                  <span className="grid-item">{sub.phone}</span>
+                  <span className="grid-item action-btn-container">
+                    <button className="custom-btn delete-btn">Delete</button>
+                  </span>
+                </div>
+              })
+            }
+          </div>
         </div>
-      </div>
-    );
+             );
+    }
   }
 }
 export default App;
--- phone-directory/src/index.js
+++ phone-directory/src/index.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
-ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister(); 
