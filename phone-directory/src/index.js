06f65a9a40d393452d8df99bae7c2c5e14abdc29
khushboo400 <khushboo171400.cse@chitkara.edu.in>
2019-12-21 00:05:49 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- phone-directory/src/index.js
+++ phone-directory/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister(); 
+import PhoneDirectory from './PhoneDirectory';
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
+serviceWorker.unregister();
